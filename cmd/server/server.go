946ce175ad62647ff7c2e6336a8b8a84eb291ea2
Matheus <mrehbein45@gmail.com>
2022-01-02 21:11:54 -0300

    adjusts found when testing server with evans

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/retatu/go-grpc/pb"
 	"github.com/retatu/go-grpc/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -18,6 +19,8 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+	reflection.Register(grpcServer)
+
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
 	}
--- services/user.go
+++ services/user.go
@@ -16,8 +16,8 @@ func NewUserService() *UserService {
 
 func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
 	return &pb.User{
-		id:    "123",
-		name:  req.GetName(),
-		email: req.GetEmail(),
+		Id:    "123",
+		Name:  req.GetName(),
+		Email: req.GetEmail(),
 	}, nil
 }
